93087863267176fe4e125c2ddd92008ea0fd30b9
Davit <iopanda65@gmail.com>
2026-06-20 00:19:09 +0400

    config: increase chunk size limit and add tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // <-- შემოგვაქვს თაილვინდი
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // <-- ვამატებთ პლაგინებში
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    // ლიმიტს ზრდის 1000 კბ-მდე, რომ Supabase-ის გამო ვორნინგი არ ამოაგდოს
+    chunkSizeWarningLimit: 1000,
+  },
 });
